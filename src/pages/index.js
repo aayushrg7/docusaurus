886eb88e206d3a2d9bac4cc9c8251df0eb940cf3
Aayush Chalise <ingoprivate@gmail.com>
2024-12-11 00:40:10 +0545

    fix intro
    Co-Authored-By: aayush <177418358+aayushrg7@users.noreply.github.com>
    Co-Authored-By: Smriti Bhandari <187972284+smritics@users.noreply.github.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--lg"
-            to="/docs/Welcome">
+            to="/docs/intro">
             Get Started
           </Link>
           <Link
